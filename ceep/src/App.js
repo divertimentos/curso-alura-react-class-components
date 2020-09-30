21faddb377a1ae64f9535f0205416005746221d8
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-09-30 16:40:07 -0300

    Cria componentes e organiza o código certinho, padronizando-o

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,40 +1,16 @@
-import React from 'react';
-function App() {
-  return (
-    <section>
-      <form>
-        <input type="text" placeholder="Título" />
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar nota</button>
-      </form>
-      <ul>
-        <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nova</p>
-          </section>
-        </li>
-        <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nova</p>
-          </section>
-        </li>
-        <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nova</p>
-          </section>
-        </li>
-      </ul>
-    </section>
-  );
+import React, { Component } from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
+
+class App extends Component() {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section >
+    );
+  }
 }
 
 export default App;
