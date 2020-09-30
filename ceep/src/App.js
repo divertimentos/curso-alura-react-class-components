3b72beace46f8fc245b12ac0a6cc5e662f551494
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-09-30 14:35:12 -0300

    WIP

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 function App() {
   return (
-   <h1>Meu app react</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar nota</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nova</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nova</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nova</p>
+          </section>
+        </li>
+      </ul>
+    </section>
   );
 }
-//react -> lib
-//React -> ecossistema
+
 export default App;
