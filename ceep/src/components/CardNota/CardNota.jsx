f3dcf0a98541d6b897261341cb4d9f9e6ee66dac
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-10-03 14:19:35 -0300

    Finaliza a aula sobre estados

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -5,10 +5,10 @@ class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
-        <header className="card-nota_cabecalho"> 
-          <h3 className="card-nota_titulo">Título</h3>
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
         </header>
-        <p className="card-nota_texto">Escreva sua nota</p>
+        <p className="card-nota_texto">{this.props.texto}</p>
       </section>
     );
   }
